e6173e300173df93b3cc69ddf6f158a7117846d2
Hanleyjaa <Hanleyjaa@DESKTOP-QFBG5NQ>
2021-10-21 02:49:02 -0400

    Add project files.

---
+++ Casino/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Casino
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mainTitle = @"
+
+
+
+  ______                       __                     
+ /      \                     /  |                    
+/$$$$$$  |  ______    _______ $$/  _______    ______  
+$$ |  $$/  /      \  /       |/  |/       \  /      \ 
+$$ |       $$$$$$  |/$$$$$$$/ $$ |$$$$$$$  |/$$$$$$  |
+$$ |   __  /    $$ |$$      \ $$ |$$ |  $$ |$$ |  $$ |
+$$ \__/  |/$$$$$$$ | $$$$$$  |$$ |$$ |  $$ |$$ \__$$ |
+$$    $$/ $$    $$ |/     $$/ $$ |$$ |  $$ |$$    $$/ 
+ $$$$$$/   $$$$$$$/ $$$$$$$/  $$/ $$/   $$/  $$$$$$/  
+                                                      
+                                                      
+                                                      
+
+
+
+
+
+";
+
+            Console.WriteLine(mainTitle);
+            Console.WriteLine("Click the any key to continue");
+            Console.ReadKey();
+            Console.Clear();
+
+
+
+            Console.WriteLine("What game would you like to play?");
+            Console.WriteLine("1. Blackjack");
+           string Input = Console.ReadLine();
+            Class1 class1 = new Class1();
+
+            if (Input == "1")
+            {
+                Console.WriteLine("blackjack");
+               
+
+            }
+            else
+            {
+                Console.WriteLine("nope");
+            }
+
+
+
+        }
+    }
+}
