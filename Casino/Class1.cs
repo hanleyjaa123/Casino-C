9405a749786b2778a1cc9c4dffeea355fe758bb7
Hanleyjaa <Hanleyjaa@DESKTOP-QFBG5NQ>
2021-10-29 05:15:33 -0400

    Added random generator (currently not in use)

--- Casino/Class1.cs
+++ Casino/Class1.cs
@@ -4,15 +4,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Casino
 {
    public class Class1
     {
 
+
+
         public void gameMechanic()
         {
-            Console.WriteLine("test002");
+            Random random = new Random();
+            for (int i = 0; i < 15; i++)
+            {
+                Console.WriteLine(random.Next(1, 10));
+            }
 
+            Console.ReadKey();
+
+
+
+        }
+
+
+         static void gameCards(int number)
+        {
+            List<int> playCards = new List<int>() { 1, 2, 5, 7, 8, 10 };
 
         }
 
--- Casino/Program.cs
+++ Casino/Program.cs
@@ -57,6 +57,7 @@ $$    $$/ $$    $$ |/     $$/ $$ |$$ |  $$ |$$    $$/
             {
                 Console.WriteLine("nope");
                 Console.ReadKey();
+                
             }
 
 
